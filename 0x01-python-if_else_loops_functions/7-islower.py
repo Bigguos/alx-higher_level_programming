9806c52d77aa7ba48125e34f1c9458f6e4d61fd2
Biggous <guosaaide10@.com>
2022-09-22 14:15:36 +0000

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-for c in range(ord("a"), ord("z")+1):
-    return True
-    for c in range(ord'A', ord'Z'+1):
-        return False
+    if ord(c) >= ord("a") and ord(c) <= ord("z"):
+        return True
+    return False
