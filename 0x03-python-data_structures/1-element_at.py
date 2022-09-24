312c263551ff239ec1ce432ba1c208cd5fb96557
Biggous <guosaaide10@.com>
2022-09-24 22:40:05 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    i = len(my_list)
     if idx < 0:
        return None
-    elif idx > len(my_list):
+    elif idx > i:
        return None
     else:
        return (my_list[idx])
