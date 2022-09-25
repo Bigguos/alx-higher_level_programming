c77416c9a8709bd51777d79e6c3f5844085f0b1f
Biggous <guosaaide10@.com>
2022-09-25 11:21:30 +0000

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list
+        my_list.insert( , )
+        i = len(my_list) - 1
+        if idx < 0:
+            return my_list
+        elif idx > i:
+            return my_list
+        else:
+            return my_list
+    
+
