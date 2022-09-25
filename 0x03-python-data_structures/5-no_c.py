5c013c48300d4d4f2f1632f1ff3f82587521a100
Biggous <guosaaide10@.com>
2022-09-25 12:00:00 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,4 @@ def no_c(my_string):
     if my_sring:
         my_string.remove(c)
         my_string.remove(C)
-        for i in my_string:
-            print("{}".format(i))
+        return my_string
