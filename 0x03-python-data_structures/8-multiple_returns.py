b1fe1e32c30935c3b2daf5fb69247156379656fb
Biggous <guosaaide10@.com>
2022-09-26 23:50:36 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     i = len(sentence)
     if i == 0:
-        new_tuple = (0, None)
+        new_tuple = (i, None)
     else:
-        new_tuple = (len(sentencw), sentence[0])
+        new_tuple = (len(i), sentence[0])
     return new_tuple
         
