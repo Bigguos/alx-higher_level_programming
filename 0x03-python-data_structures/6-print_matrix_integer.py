ee0706b23d8d2e43451e295cf41ce061a39c94da
Biggous <guosaaide10@.com>
2022-09-26 16:20:29 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        a = 1
+        for j in i:
+            if a == len(i):
+                print("{:d}".format(j). end="")
+            else:
+                print("{:d}".format(j). end="")
+            a = a + 1
+        print()
