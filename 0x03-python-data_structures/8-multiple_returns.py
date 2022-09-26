3871fca0c5dd2d12822be8fd6add07bf2c3e2a3d
Biggous <guosaaide10@.com>
2022-09-26 23:47:06 +0000

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    i = len(sentence):
+    if i == 0:
+        new_tuple = (0, None)
+    else:
+        new_tuple = (len(sentencw), sentence[0])
+    return new_tuple
+        
