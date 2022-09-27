b81b21376ee71f246321c358866bb6d954222bbd
Biggous <guosaaide10@.com>
2022-09-27 00:42:10 +0000

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = []
     for i in my_list:
         if (i % 2 == 1):
-            return False
+            new_list.append(True)
         if (i % 2 == 0):
-            return True
-        else:
-            new_list = i
-            return new_list
+            new_list.append(True)
+    return new_list
