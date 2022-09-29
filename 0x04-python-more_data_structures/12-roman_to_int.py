8d7b6d5b9181f9ffd2fd7fdfb4af69a00996f788
Biggous <guosaaide10@.com>
2022-09-29 21:52:18 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,53 +1,15 @@
 #!/usr/bin/python3
-def _get_value(char):
-  """
-    Returns the roman value of a character
-    None if its not a Roman Character
-    """
-    romans = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 100
-    }
-    char = char.upper()
-    if char in romans:
-        return romans[char]
-    return None
+def roman_to_int(roman_string):
 
-def roman_to_int(roman):
-    """
-    Converts a roman numerals to Decimal
-    Args:
-        roman - the string f roman numerals
-    """
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
 
-    if not isinstance(roman, str) or roman is None:
-        return 0
-
-    result, prev, cur = 0, 0, 0
-
-    for c in roman:
-        cur = _get_value(c)
-        if cur is None:
-            raise ValueError("Wrong input")
-        if cur > prev:
-            result -= prev
-            cur -= prev
-        result += cur
-        prev = cur
-    return result
-
-if __name__ == '__main__':
-    tests = [
-        'X',
-        'VII',
-        'IX',
-        'LXXVII',
-        'DCCVII'
-    ]
-    for test in tests:
-        print(test, '=', roman_to_int(test))
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[c]] >= p:
+                res += val[roman_string[c]]
+            else:
+                res -= val[roman_string[c]]
+            p = val[roman_string[c]]
+    return res
