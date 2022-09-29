960e76f86595142fd6adf74e6f1ae6c323adc48b
Biggous <guosaaide10@.com>
2022-09-29 14:04:31 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
-    for i in new_matrix:
+def square_matrix_simple(matrix=[])
+    for i in matrix:
          return i ** 2
 
